da425c173b402800c74d7081c60ace6ab78a4d93
ffashion <1323579954@qq.com>
2021-01-18 12:33:28 +0800

    disable client terminal ECHO

--- rsh_client.c
+++ rsh_client.c
@@ -5,7 +5,43 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <termio.h>
 #include "rsh_client.h"
+struct termios original_termios;
+int tty_set_raw(int fd, struct termios *prevTermios)
+{
+    struct termios t;
+
+    if (tcgetattr(fd, &t) == -1)
+        return -1;
+
+    if (prevTermios != NULL)
+        *prevTermios = t;
+
+    t.c_lflag &= ~(ICANON | ISIG | IEXTEN | ECHO);
+                        /* Noncanonical mode, disable signals, extended
+                           input processing, and echoing */
+
+    t.c_iflag &= ~(BRKINT | ICRNL | IGNBRK | IGNCR | INLCR |
+                      INPCK | ISTRIP | IXON | PARMRK);
+                        /* Disable special handling of CR, NL, and BREAK.
+                           No 8th-bit stripping or parity error handling.
+                           Disable START/STOP output flow control. */
+
+    t.c_oflag &= ~OPOST;                /* Disable all output processing */
+
+    t.c_cc[VMIN] = 1;                   /* Character-at-a-time input */
+    t.c_cc[VTIME] = 0;                  /* with blocking */
+
+    if (tcsetattr(fd, TCSAFLUSH, &t) == -1)
+        return -1;
+
+    return 0;
+}
+void reduct_termios(void){
+    tcsetattr(STDIN_FILENO,TCSANOW,&original_termios);
+}
+
 int connect2server(char *addr,int port){
     struct sockaddr_in client = {0};
     int client_fd;
@@ -26,6 +62,7 @@ int main(int argc,char *argv[]){
     char *buf = (char *)malloc(BUF_SIZE);
     char *addr = malloc(100);
     int port;
+    struct termios modified_termios;
 
     if(argc == 1){
         // printf("Usage: ./client [ip] [port]\n");
@@ -40,8 +77,13 @@ int main(int argc,char *argv[]){
         port = atoi(argv[2]);
         
     }
+    //客户端和服务器端选择关闭一个回显即可 这里选择关闭客户端回显 然后在客户端退出之后 再次打开回显
+    tcgetattr(STDIN_FILENO,&original_termios);
+    atexit(reduct_termios);
+    tty_set_raw(STDIN_FILENO,&modified_termios);
 
     client_fd = connect2server(addr,port);
+
 
     while(1){
         
